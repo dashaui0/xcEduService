52b50d4eb2e8c9fb3c603a67f1082b69b0e553f0
DaShuai <dashuai_0000@163.com>
2020-12-22 10:00:39 +0800

    学成项目初始化

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -0,0 +1,21 @@
+package com.xuecheng.manage_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author:dashuai
+ * @ClassName:ManageCmsApplication
+ * @Descirption:
+ * @date:2020/12/22 9:55
+ */
+@SpringBootApplication
+@EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
+@ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
+public class ManageCmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCmsApplication.class,args);
+    }
+}
