96d72b7f1339ccbd2a96496306dbc553caeb5fc8
DaShuai <dashuai_0000@163.com>
2020-12-26 18:48:22 +0800

    添加了查询页面接口

--- xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
+++ xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
@@ -1,5 +1,6 @@
 package com.xuecheng.framework.domain.cms.request;
 
+import com.xuecheng.framework.model.request.RequestData;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -9,19 +10,23 @@ import lombok.Data;
  * @create 2018-09-12 14:59
  **/
 @Data
-public class QueryPageRequest {
+public class QueryPageRequest extends RequestData{
     //接收页面查询的查询条件
     //站点id
     //站点id
     @ApiModelProperty("站点id")
     private String siteId;
     //页面ID
+    @ApiModelProperty("页面ID")
     private String pageId;
     //页面名称
+    @ApiModelProperty("页面名称")
     private String pageName;
     //别名
+    @ApiModelProperty("别名")
     private String pageAliase;
     //模版id
+    @ApiModelProperty("模版id")
     private String templateId;
     //....
 }
--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -1,7 +1,16 @@
 package com.xuecheng.manage_cms.service;
 
+import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /**
@@ -10,17 +19,38 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Descirption:
  * @date:2020/12/23 16:06
  */
+@Service
 public class PageService {
+
+    @Autowired
+    private CmsPageRepository cmsPageRepository;
 
     /**
      * 页面查询方法
-     *
-     * @param page
-     * @param size
-     * @param queryPageRequest
-     * @return
+     * @param page 当前页码
+     * @param size 页面显示个数
+     * @param queryPageRequest 查询条件
+     * @return 页面列表
      */
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
-        return null;
+        if (queryPageRequest == null){
+            queryPageRequest = new QueryPageRequest();
+        }
+        if (page <= 0){
+            page = 1;
+        }
+        page = page -1; //为了适应mongdb的接口将页码-1
+        if (size <= 0){
+            size = 10;
+        }
+        //分页对象
+        Pageable pageable= PageRequest.of(page,size);
+        //分页查询
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        QueryResult<CmsPage> cmsPageQueryResult = new QueryResult<>();
+        cmsPageQueryResult.setList(all.getContent());
+        cmsPageQueryResult.setTotal(all.getTotalElements());
+        //返回结果
+        return new QueryResponseResult(CommonCode.SUCCESS,cmsPageQueryResult);
     }
 }
