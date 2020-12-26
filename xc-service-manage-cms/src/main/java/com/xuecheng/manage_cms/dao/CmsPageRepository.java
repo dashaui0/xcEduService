ace959c8b50d76182a8a43085e384c09e8afd142
DaShuai <dashuai_0000@163.com>
2020-12-26 21:12:56 +0800

    添加新增页面方法

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
@@ -4,6 +4,13 @@ import com.xuecheng.framework.domain.cms.CmsPage;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CmsPageRepository extends MongoRepository<CmsPage,String> {
-
+    /**
+     * 根据页面名称、站点id、页面webpath查询页面方法，此方法用于校验页面是否存在
+     * @param pageName
+     * @param siteId
+     * @param pageWebPath
+     * @return
+     */
+    public CmsPage findByPageNameAndSiteIdAndPageWebPath(String pageName,String siteId,String pageWebPath);
 
 }
--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -2,6 +2,7 @@ package com.xuecheng.manage_cms.service;
 
 import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
 import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
@@ -26,8 +27,8 @@ public class PageService {
 
     /**
      * 页面查询方法
-     * @param page 当前页码
-     * @param size 页面显示个数
+     * @param page             当前页码
+     * @param size             页面显示个数
      * @param queryPageRequest 查询条件
      * @return 页面列表
      */
@@ -35,36 +36,57 @@ public class PageService {
         //条件匹配器
         //页面名称模糊查询，需要自定义字符串的匹配器实现模糊查询
         ExampleMatcher exampleMatcher = ExampleMatcher.matching()
-                .withMatcher("pageAliase",ExampleMatcher.GenericPropertyMatchers.contains());
+                .withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
         //条件值
         CmsPage cmsPage = new CmsPage();
         //站点ID
-        if (StringUtils.isNotEmpty(queryPageRequest.getSiteId())){
+        if (StringUtils.isNotEmpty(queryPageRequest.getSiteId())) {
             cmsPage.setSiteId(queryPageRequest.getSiteId());
         }
         //页面别名
-        if (StringUtils.isNotEmpty(queryPageRequest.getPageAliase())){
+        if (StringUtils.isNotEmpty(queryPageRequest.getPageAliase())) {
             cmsPage.setPageAliase(queryPageRequest.getPageAliase());
         }
 
         //创建实例条件
         Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
         //
-        if (page <= 0){
+        if (page <= 0) {
             page = 1;
         }
-        page = page -1; //为了适应mongdb的接口将页码-1
-        if (size <= 0){
+        page = page - 1; //为了适应mongdb的接口将页码-1
+        if (size <= 0) {
             size = 10;
         }
         //分页对象
-        Pageable pageable= PageRequest.of(page,size);
+        Pageable pageable = PageRequest.of(page, size);
         //分页查询
-        Page<CmsPage> all = cmsPageRepository.findAll(example,pageable);
+        Page<CmsPage> all = cmsPageRepository.findAll(example, pageable);
         QueryResult<CmsPage> cmsPageQueryResult = new QueryResult<>();
         cmsPageQueryResult.setList(all.getContent());
         cmsPageQueryResult.setTotal(all.getTotalElements());
         //返回结果
-        return new QueryResponseResult(CommonCode.SUCCESS,cmsPageQueryResult);
+        return new QueryResponseResult(CommonCode.SUCCESS, cmsPageQueryResult);
+    }
+
+    /**
+     * 添加页面
+     * @param cmsPage
+     * @return
+     */
+    public CmsPageResult add(CmsPage cmsPage) {
+        //校验页面是否存在，根据页面名称，站点Id、页面webpath查询
+        CmsPage cmsPage1 = cmsPageRepository.findByPageNameAndSiteIdAndPageWebPath(cmsPage.getPageName(), cmsPage.getSiteId(), cmsPage.getPageWebPath());
+        if (cmsPage1 == null) {
+            //添加页面主键，由spring data自动生成
+            cmsPage.setPageId(null);
+            cmsPageRepository.save(cmsPage);
+            //返回结果
+            CmsPageResult cmsPageResult = new CmsPageResult(CommonCode.SUCCESS, cmsPage);
+            return cmsPageResult;
+        }
+
+        return new CmsPageResult(CommonCode.FAIL,null);
+
     }
 }
